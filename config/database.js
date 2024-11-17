ee68dfe14299b7f2be974c438062722fac92103b
Juan Diego <juandiroca@hotmail.com>
2024-11-17 09:54:49 -0500

    cambio prueba en database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
-// config/database.js
 const { Sequelize } = require('sequelize');
 
-// Usa el endpoint de RDS como host
-const sequelize = new Sequelize('database-1', 'admin', 'password', {
-    host: 'database-1.cggv94uga47a.us-east-1.rds.amazonaws.com', // Reemplázalo con tu endpoint de RDS
-    dialect: 'mysql'
+const sequelize = new Sequelize('ecommerceDB', 'admin', 'password', {
+    host: 'database-1.crg8mcswkrzj.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    dialectOptions: {
+        connectTimeout: 10000  // 10 segundos
+    }
 });
 
+
 module.exports = sequelize;
+
 
