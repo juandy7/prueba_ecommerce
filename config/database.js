93a0db6e0b78c242d2d34f1e53352b2e810a83e8
Juan Diego <juandiroca@hotmail.com>
2024-11-16 19:29:22 -0500

    cambio para despliegue

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 // Usa el endpoint de RDS como host
-const sequelize = new Sequelize('database1', 'admin', 'password', {
+const sequelize = new Sequelize('database-1', 'admin', 'password', {
     host: 'database-1.cggv94uga47a.us-east-1.rds.amazonaws.com', // Reemplázalo con tu endpoint de RDS
     dialect: 'mysql'
 });
