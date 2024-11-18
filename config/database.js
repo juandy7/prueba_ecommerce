619ef4a54e51cfd484bab1831d102cd2efc526ca
Juan Diego Rosero <103261154+juandy7@users.noreply.github.com>
2024-11-17 21:13:58 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('ecommerceDB', 'admin', 'password', {
-    host: ' ecommercedb.cggv94uga47a.us-east-1.rds.amazonaws.com',
+    host: 'ecommercedb.cggv94uga47a.us-east-1.rds.amazonaws.com',
     dialect: 'mysql'
 });
 
