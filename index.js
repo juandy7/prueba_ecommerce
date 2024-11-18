254ab1d591920de9e4609eb3672a22dd574ff855
Juan Diego Rosero <103261154+juandy7@users.noreply.github.com>
2024-11-17 19:43:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,26 @@ app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Servir archivos estáticos (si tienes un directorio "public" con HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta raíz para servir el archivo HTML (si tienes un index.html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Asegúrate de tener el archivo 'index.html' en la carpeta 'public'
+});
+
 // Usamos las rutas
 app.use('/auth', authRoutes); // Rutas de autenticación que se conecta con authRoutes
 app.use('/products', productRoutes); // Rutas de productos que se conecta con productRoutes
-app.use('/cart', cartRoutes); // Rutas de productos que se conecta con productRoutes
+app.use('/cart', cartRoutes); // Rutas de productos que se conecta con cartRoutes
 
+// Conectar a la base de datos
 sequelize.sync()
     .then(() => console.log('Base de datos sincronizada'))
     .catch(error => console.log('Error al sincronizar la DB:', error));
 
+// Iniciar el servidor
 app.listen(3000, () => {
     console.log('Servidor en ejecución en el puerto 3000');
 });
+
